6105ab1f7b17bc973f0947782b39f7db6cae5365
montre <113481566+rentaro-m-b@users.noreply.github.com>
2025-10-03 22:49:04 +0900

    feat: creating health api

--- src/main/java/com/example/for_reusable_testcontainer/ForReusableTestcontainerApplication.java
+++ src/main/java/com/example/for_reusable_testcontainer/ForReusableTestcontainerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ForReusableTestcontainerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ForReusableTestcontainerApplication.class, args);
 	}
-
 }
--- src/test/java/com/example/for_reusable_testcontainer/it/health/HealthApiTest.java
+++ src/test/java/com/example/for_reusable_testcontainer/it/health/HealthApiTest.java
@@ -1,15 +1,26 @@
 package com.example.for_reusable_testcontainer.it.health;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static io.restassured.RestAssured.when;
+import static org.apache.catalina.util.XMLWriter.NO_CONTENT;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class HealthApiTest {
+    @BeforeEach
+    public void beforeEach() {
+        RestAssured.baseURI = "http://localhost/api";
+    }
+
     @Test
     public void normal() {
         when().
-            get("/health").
-            then().
-            statusCode(204);
+                get("/health").
+                then().
+                statusCode(NO_CONTENT)
+        ;
     }
 }
