ba821a4f3d52b402a13dbc8a7023ba17ebe5e66a
montre <113481566+rentaro-m-b@users.noreply.github.com>
2025-10-05 22:54:16 +0900

    feat: creating file to start up for it

---
+++ src/test/java/com/example/for_reusable_testcontainer/StartUpApplication.java
@@ -0,0 +1,11 @@
+package com.example.for_reusable_testcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ForReusableTestcontainerApplication.class, args);
+    }
+}
--- src/test/java/com/example/for_reusable_testcontainer/it/health/HealthApiTest.java
+++ src/test/java/com/example/for_reusable_testcontainer/it/health/HealthApiTest.java
@@ -1,17 +1,17 @@
 package com.example.for_reusable_testcontainer.it.health;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static io.restassured.RestAssured.when;
 import static org.apache.catalina.util.XMLWriter.NO_CONTENT;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 public class HealthApiTest {
-    @BeforeEach
-    public void beforeEach() {
+    @BeforeAll
+    static void beforeEach() {
         RestAssured.baseURI = "http://localhost/api";
     }
 
