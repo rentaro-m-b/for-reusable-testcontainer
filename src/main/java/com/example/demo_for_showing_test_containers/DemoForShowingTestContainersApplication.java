0102c72d429ec3e505b972686df93829218d6139
montre <113481566+rentaro-m-b@users.noreply.github.com>
2025-10-07 00:37:01 +0900

    feat: mysql

--- src/main/java/com/example/demo_for_showing_test_containers/DemoForShowingTestContainersApplication.java
+++ src/main/java/com/example/demo_for_showing_test_containers/DemoForShowingTestContainersApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoForShowingTestContainersApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoForShowingTestContainersApplication.class, args);
 	}
-
 }
