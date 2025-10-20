db6fe5e85eed4aa23078523d97931be5293a2595
montre <113481566+rentaro-m-b@users.noreply.github.com>
2025-10-20 23:29:05 +0900

    fix: it

--- src/test/java/com/example/demo_for_showing_test_containers/health/GetHealthAPITest.java
+++ src/test/java/com/example/demo_for_showing_test_containers/health/GetHealthAPITest.java
@@ -4,30 +4,12 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.images.builder.ImageFromDockerfile;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-import java.nio.file.Path;
-import java.time.Duration;
 
 import static io.restassured.RestAssured.given;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 
-@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GetHealthAPITest {
-    @Container
-    public static GenericContainer<?> app =
-            new GenericContainer<>(
-                    new ImageFromDockerfile(".")
-                            .withDockerfilePath("Dockerfile")
-            )
-                    .withExposedPorts(8080)
-                    .waitingFor(Wait.forHttp("/health").forStatusCode(204));
-
     @BeforeAll
     static void setUp() {
         RestAssured.baseURI = "http://localhost";
@@ -35,7 +17,6 @@ class GetHealthAPITest {
     }
 
     @Test void getHealth() {
-        // given().get("/health").then().statusCode(NO_CONTENT.value());
-        System.out.println("");
+        given().get("/health").then().statusCode(NO_CONTENT.value());
     }
 }
